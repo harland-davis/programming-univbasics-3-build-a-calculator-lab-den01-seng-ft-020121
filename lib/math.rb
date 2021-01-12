be7e8b844ae1a0689a776fba2d00216bacec9d5e
Kyle Davis <davis.h.kyle@gmail.com>
2021-01-12 01:37:03 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -3,11 +3,11 @@ num2 + num1
 end
 
 def subtraction(num1, num2)
-num2-num1
+num2 - num1
 end
 
 def division(num1, num2)
-puts(num2/num1)
+num2/num1
 end
 
 def multiplication(num1, num2)
@@ -15,9 +15,9 @@ num2 * num1
 end
 
 def modulo(num1, num2)
-modulo = num2 % num1 
+puts num2 % num1 
 end
 
 def square_root(num)
-num**num
+puts num**num
 end
